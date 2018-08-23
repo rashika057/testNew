d426a9946f93e352b065c5518cc7f1d2b2d2e606
Rashika Shukla <rashika059@gmail.com>
2018-08-23 15:26:26 +0530

    Second commit

--- test/src/test/testCollection.java
+++ test/src/test/testCollection.java
@@ -1,9 +1,15 @@
 package test;
 
+import java.util.HashSet;
+
 public class testCollection {
 
 	public static void main(String[] args)
 	{
 		System.out.print("Hello");
+		HashSet<Integer> br=new HashSet<>();
+		br.add(10);
+		br.add(15);
+		
 	}
 }
