5ec7cdbb864d28b68aaee86fa4715b37c5b0ad54
Rashika Shukla <rashika059@gmail.com>
2018-08-23 12:04:18 +0530

    some changes

---
+++ test/src/test/Employee.java
@@ -0,0 +1,13 @@
+package test;
+
+public class Employee {
+	int employeeID;
+	String name;
+	int age;
+	Employee(int employeeID,String name,int age)
+	{
+		this.employeeID=employeeID;
+		this.name=name;
+		this.age=age;
+	}
+}
